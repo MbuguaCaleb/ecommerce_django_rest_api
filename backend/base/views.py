c24c0bec65135dc13162dfc69bfa2b3aaf6194c9
MbuguaCaleb <mbuguacaleb30@gmail.com>
2021-03-06 22:54:20 +0300

    user-registration

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
@@ -10,9 +9,11 @@ from .serializers import ProductSerializer, UserSerializer, UserSerializerWithTo
 # Create your views here.
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
-
-
+from django.contrib.auth.hashers import make_password
+from rest_framework import status
 # token customization
+
+
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     # @classmethod   # Customises the data inside the JWT
 
@@ -41,23 +42,29 @@ class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
 
 
-@api_view(['GET'])
-def getRoutes(request):
-    routes = [
-        '/api/products/',
-        '/api/products/create',
-        '/api/products/upload',
-        '/api/products/<id>/reviews/',
-        '/api/products/top/',
-        '/api/products/<id>/',
-        '/api/products/delete/<id>',
-        '/api/products/<update>/<id>/',
-    ]
+@api_view(['POST'])
+def registerUser(request):
+    data = request.data
 
-    return Response(routes)
+    try:
+        user = User.objects.create(
+            first_name=data['name'],
+            username=data['email'],
+            email=data['email'],
+            password=make_password(data['password'])
+        )
+
+       # because i am only returning only one user
+        serializer = UserSerializerWithToken(user, many=False)
+        return Response(serializer.data)
+
+    except:
+        message = {'detail': 'User with this email already exists'}
+        return Response(message, status=status.HTTP_400_BAD_REQUEST)
+
+        # Get user profile
 
 
-# Get user profile
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def getUserProfile(request):
