a3e6e759d8bd87859fa42baf052b8439933beac4
MbuguaCaleb <mbuguacaleb30@gmail.com>
2021-03-06 10:48:38 +0300

    routes-protection

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,29 +1,15 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from .jsondata import users
-from .models import Product, User
+from .models import Product
+from django.contrib.auth.models import User
 from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
 # Create your views here.
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
-
-
-@api_view(['GET'])
-def getRoutes(request):
-    routes = [
-        '/api/products/',
-        '/api/products/create',
-        '/api/products/upload',
-        '/api/products/<id>/reviews/',
-        '/api/products/top/',
-        '/api/products/<id>/',
-        '/api/products/delete/<id>',
-        '/api/products/<update>/<id>/',
-    ]
-
-    return Response(routes)
 
 
 # token customization
@@ -56,12 +42,41 @@ class MyTokenObtainPairView(TokenObtainPairView):
 
 
 @api_view(['GET'])
+def getRoutes(request):
+    routes = [
+        '/api/products/',
+        '/api/products/create',
+        '/api/products/upload',
+        '/api/products/<id>/reviews/',
+        '/api/products/top/',
+        '/api/products/<id>/',
+        '/api/products/delete/<id>',
+        '/api/products/<update>/<id>/',
+    ]
+
+    return Response(routes)
+
+
+# Get user profile
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def getUserProfile(request):
     user = request.user
     serializer = UserSerializer(user, many=False)
     return Response(serializer.data)
 
 
+# Get all the system users
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def getUsers(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
+
+    return Response(serializer.data)
+
+
+# get all Products
 @api_view(['GET'])
 def getProducts(request):
     products = Product.objects.all()
@@ -71,9 +86,9 @@ def getProducts(request):
     return Response(serializer.data)
 
 
+# get Single Products
 @api_view(['GET'])
 def getProduct(request, pk):
-
     product = Product.objects.get(_id=pk)
     # when returning one object many is set to false
     serializer = ProductSerializer(product, many=False)
